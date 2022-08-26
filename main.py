3c387c3214e61bc51368dba9eadcf3521a9122fe
jnsbck-uni <jonas.beck@uni-tuebingen.de>
2022-08-26 17:00:04 +0200

    fix: add license to main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# This file is part of pyqueue, a simple slurm like job queue written in python. pyqueue is licensed
+# under the GNU General Public License v3, see <https://www.gnu.org/licenses/>. Copyright 2022 Jonas Beck
+
 from pyqueue.client import main
 if __name__ == "__main__":
     main()
